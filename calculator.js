2704d801d49693fc1507da8f14aef1ad1ec82d58
kshitij <kshitij22999@gmail.com>
2025-01-18 03:39:57 +0530

    added custom delimiter

--- calculator.js
+++ calculator.js
@@ -1,7 +1,25 @@
 class Calculator {
-    calculate(input) {
+    add(input) {
         if (!input) return 0;
-        return input.split('+').reduce((sum, num) => sum + parseInt(num, 10), 0);
+        let numbers = [];
+        if (input.slice(0,2) == '//'){
+            numbers = input
+                .slice(4)
+                .split(input[2])
+        }
+        else{
+            numbers =  input.split(/,|\n/)
+        }
+
+        const negativeNumbers = numbers.filter(num => num < 0);
+        if (negativeNumbers.length > 0) {
+            throw new Error(`Negatives not allowed: ${negativeNumbers.join(', ')}`);
+        }
+
+        return numbers
+            .map(num => parseInt(num.trim(), 10))
+            .filter(num => !isNaN(num))
+            .reduce((sum, num) => sum + num, 0);
     }
 }
 
--- calculator.test.js
+++ calculator.test.js
@@ -8,18 +8,38 @@ describe('String-Based Calculator', () => {
     });
 
     test('should return 0 for an empty string', () => {
-        expect(calc.calculate('')).toBe(0);
+        expect(calc.add('')).toBe(0);
     });
 
     test('should return the number itself for a single number input', () => {
-        expect(calc.calculate('5')).toBe(5);
+        expect(calc.add('5')).toBe(5);
     });
 
-    test('should return the sum of two numbers separated by "+"', () => {
-        expect(calc.calculate('2+3')).toBe(5);
+    test('should return the sum of two numbers separated by ","', () => {
+        expect(calc.add('2,3')).toBe(5);
     });
 
-    test('should return the sum of multiple numbers separated by "+"', () => {
-        expect(calc.calculate('1+2+3')).toBe(6);
+    test('should return the sum of multiple numbers separated by ","', () => {
+        expect(calc.add('1,2,3,4')).toBe(10);
+    });
+
+    test('should accept \n as a delimeters', () => {
+        expect(calc.add('1\n2\n3')).toBe(6);
+    });
+
+    test('should accept \n and , as a delimeters', () => {
+        expect(calc.add('1\n2,3')).toBe(6);
+    });
+
+    test('should handle custom delimeters', () => {
+        expect(calc.add('//;\n1;2')).toBe(3);
+    });
+
+    test('should handle custom delimeters', () => {
+        expect(calc.add('//*\n10*2')).toBe(12);
+    });
+
+    test('should throw an error for negative numbers', () => {
+        expect(() => calc.add('-2,3')).toThrow(Error);
     });
 });
