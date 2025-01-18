e39ec960f72328523d8c68cb05532fbd2e564035
kshitij <kshitij22999@gmail.com>
2025-01-18 22:59:17 +0530

    added multiplication and subtraction

--- calculator.js
+++ calculator.js
@@ -21,6 +21,52 @@ class Calculator {
             .filter(num => !isNaN(num))
             .reduce((sum, num) => sum + num, 0);
     }
+
+    multiply(input) {
+        if (!input) return 0;
+        let numbers = [];
+        if (input.slice(0,2) == '//'){
+            numbers = input
+                .slice(4)
+                .split(input[2])
+        }
+        else{
+            numbers =  input.split(/,|\n/)
+        }
+
+        const negativeNumbers = numbers.filter(num => num < 0);
+        if (negativeNumbers.length > 0) {
+            throw new Error(`Negatives not allowed: ${negativeNumbers.join(', ')}`);
+        }
+
+        return numbers
+            .map(num => parseInt(num.trim(), 10))
+            .filter(num => !isNaN(num))
+            .reduce((multiply, num) => multiply * num);
+    }
+
+    subtract(input) {
+        if (!input) return 0;
+        let numbers = [];
+        if (input.slice(0,2) == '//'){
+            numbers = input
+                .slice(4)
+                .split(input[2])
+        }
+        else{
+            numbers =  input.split(/,|\n/)
+        }
+
+        const negativeNumbers = numbers.filter(num => num < 0);
+        if (negativeNumbers.length > 0) {
+            throw new Error(`Negatives not allowed: ${negativeNumbers.join(', ')}`);
+        }
+
+        return numbers
+            .map(num => parseInt(num.trim(), 10))
+            .filter(num => !isNaN(num))
+            .reduce((sum, num) => sum - num);
+    }
 }
 
 module.exports = Calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -42,4 +42,68 @@ describe('String-Based Calculator', () => {
     test('should throw an error for negative numbers', () => {
         expect(() => calc.add('-2,3')).toThrow(Error);
     });
+
+    test('should return 0 for an empty string in multiply', () => {
+        expect(calc.multiply('')).toBe(0);
+    });
+
+    test('should return the number itself for a single number input in multiply', () => {
+        expect(calc.multiply('5')).toBe(5);
+    });
+
+    test('should return the sum of two numbers separated by "," in multiply', () => {
+        expect(calc.multiply('2,3')).toBe(6);
+    });
+
+    test('should return the sum of multiple numbers separated by "," in multiply', () => {
+        expect(calc.multiply('1,2,3,4')).toBe(24);
+    });
+
+    test('should accept \n as a delimeters in multiply', () => {
+        expect(calc.multiply('1\n2\n3')).toBe(6);
+    });
+
+    test('should accept \n and , as a delimeters in multiply', () => {
+        expect(calc.multiply('1\n2,3')).toBe(6);
+    });
+
+    test('should handle custom delimeters in multiply', () => {
+        expect(calc.multiply('//;\n1;2')).toBe(2);
+    });
+
+    test('should handle custom delimeters in multiply', () => {
+        expect(calc.multiply('//*\n10*2')).toBe(20);
+    });
+
+    test('should return 0 for an empty string in multiply', () => {
+        expect(calc.subtract('')).toBe(0);
+    });
+
+    test('should return the number itself for a single number input in multiply', () => {
+        expect(calc.subtract('5')).toBe(5);
+    });
+
+    test('should return the sum of two numbers separated by "," in multiply', () => {
+        expect(calc.subtract('2,3')).toBe(-1);
+    });
+
+    test('should return the sum of multiple numbers separated by "," in multiply', () => {
+        expect(calc.subtract('1,2,3,4')).toBe(-8);
+    });
+
+    test('should accept \n as a delimeters in multiply', () => {
+        expect(calc.subtract('1\n2\n3')).toBe(-4);
+    });
+
+    test('should accept \n and , as a delimeters in multiply', () => {
+        expect(calc.subtract('1\n2,3')).toBe(-4);
+    });
+
+    test('should handle custom delimeters in multiply', () => {
+        expect(calc.subtract('//;\n1;2')).toBe(-1);
+    });
+
+    test('should handle custom delimeters in multiply', () => {
+        expect(calc.subtract('//*\n10*2')).toBe(8);
+    });
 });
